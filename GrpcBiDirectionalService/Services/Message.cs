75dcf5ce6a53de1c90dd0e7b3222bbf2aabebdf7
alifuatyildirim <alifuat.yildirim@hotmail.com>
2022-08-01 13:35:39 +0300

    first commit

---
+++ GrpcBiDirectionalService/Services/Message.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using static GrpcBiDirectionalService.Message;
+namespace GrpcBiDirectionalService.Services
+{
+    public class MessageService:MessageBase
+    {
+        public async override Task GetMessage(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            Task response = Task.Run(async () =>
+            {
+                int count = 0;
+                while (++count <= 10)
+                {
+                    await responseStream.WriteAsync(new MessageResponse { Message = $"{count}. İstek alındı ve işlendi..." });
+                    await Task.Delay(1000);
+                }
+            });
+
+            Task request = Task.Run(async () =>
+            {
+                while (await requestStream.MoveNext())
+                {
+                    Console.WriteLine($"Mesaj alınmıştır.");
+                    Console.WriteLine("Gelen mesaj : ");
+                    Console.WriteLine(requestStream.Current.Message);
+                }
+            });
+
+            await response;
+            await request;
+        }
+    }
+}
---
+++ gRPCBiDirectionalClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Grpc.Net.Client; 
+using GrpcBiDirectionalClient; 
+using System.Threading;
+using System.Threading.Tasks;
+namespace gRPCBiDirectionalClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var messageClient= new Message.MessageClient(channel);
+            Console.WriteLine("Lütfen gönderilecek mesajı giriniz.");
+            var getMessage = messageClient.GetMessage();
+
+            Task request = Task.Run(async () =>
+            {
+                int count = 0;
+                while (++count <= 10)
+                {
+                    await getMessage.RequestStream.WriteAsync(new MessageRequest { Message = $"Gönderilen mesaj {count}" });
+                    await Task.Delay(1000);
+                }
+            });
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            Task response = Task.Run(async () =>
+            {
+                while (await getMessage.ResponseStream.MoveNext(cancellationTokenSource.Token))
+                    Console.WriteLine(getMessage.ResponseStream.Current.Message);
+            });
+            await request;
+            //RequestStream başarıyla sonlandırılıyor. Özellikle request'ten sonra olmasına özen gösteriyoruz.
+            await getMessage.RequestStream.CompleteAsync();
+            await response;
+        }
+    }
+}
